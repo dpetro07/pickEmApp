ace914c46b0401704536249e90746b89a5cdd0c6
Daniel Petro <DanielPetro@Daniels-MacBook-Pro.local>
2016-05-27 13:26:33 -0400

    basic if

---
+++ weeks.js
@@ -0,0 +1,89 @@
+// WEEK1
+//Panthers v Broncos
+ if(panthers > broncos) {
+  console.log("Panthers Win!")
+ }
+ else {
+  console.log("Broncos Win!");
+ }
+//Vikings v Titans
+ if(vikings > titans) {
+  console.log("Panthers Win!")
+ }
+ else {
+  console.log("Vikings Win!");
+ }
+//Browns v Eagles
+
+//Chargers v Chiefs
+
+//Buccaneers v Falcons
+
+//Packers v Jaguars
+
+//Bears v Texans
+
+//Bills v Ravens
+
+//Raiders v Saints
+
+//Bengals v Jets
+
+//Dolphins v Seahawks
+
+//Giants v Cowboys
+
+//Lions v Colts
+
+//Patriots v Cardinals
+
+
+// WEEK2
+
+
+// WEEK3
+
+// WEEK4
+
+
+// WEEK5
+
+
+// WEEK6
+
+
+// WEEK7
+
+
+// WEEK8
+
+
+// WEEK9
+
+
+// WEEK10
+
+
+// WEEK11
+
+
+// WEEK12
+
+
+// WEEK13
+
+
+// WEEK14
+
+
+// WEEK15
+
+
+// WEEK16
+
+
+// WEEK17
+
+
+
+
